c7f31bf05aabfaf87afcf9812a60655e0d4b9ddc
Daniel Toll <daniel@familjentoll.se>
2014-02-03 08:34:05 +0100

    first commit

---
+++ tests/se/lnu/daniel/application/findProjects.java
@@ -0,0 +1,33 @@
+package se.lnu.daniel.application;
+
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class findProjects {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testApplication() {
+		
+	}
+
+}
